395f76354bb11db40d225f8492bc65fed9e11ded
naofumi-fujii <naofumi.fujii@gmail.com>
2022-06-03 22:45:01 +0900

    -r

--- cmd.sh
+++ cmd.sh
@@ -1,1 +1,1 @@
-cat hoge.json | jq '(add | keys), ((add | keys | map({(.):null}) | add )+.[] | [.[]]) | @csv'
+cat hoge.json | jq -r '(add | keys), ((add | keys | map({(.):null}) | add )+.[] | [.[]]) | @csv'
